caaac8ef082ee9fb58740b3ba9db52921be554ba
Leonardo Pinheiro <leo.r.pinheiro@ulbra.edu.br>
2018-09-13 19:34:40 -0300

    add (user) auth middleware

--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,20 @@ const requireDir = require('require-dir');
 
 const routes = express.Router();
 
+const authMiddleware = require('./middlewares/auth');
+
 const controllers = requireDir('./controllers');
 
 // Auth
 routes.post('/signup', controllers.authController.signup);
 routes.post('/signin', controllers.authController.signin);
+
+// Auth routes
+routes.use(authMiddleware);
+
+routes.get('/tweets', (req, res) => {
+  console.log(req.userId);
+  return res.send('OK');
+});
 
 module.exports = routes;
