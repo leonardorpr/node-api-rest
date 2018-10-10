bc78babc13dc89420abf6e6cfcd6ff232924753d
Leonardo Pinheiro <leo.r.pinheiro@ulbra.edu.br>
2018-10-10 13:23:37 -0300

    add (*) configure nodemailer e create email template

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const sendMail = require('../services/mailer');
 
 const User = mongoose.model('User');
 
@@ -12,6 +13,17 @@ module.exports = {
       }
 
       const user = await User.create(req.body);
+
+      sendMail({
+        from: 'Leonardo Pinheiro <leoprocha100@gmail.com>',
+        to: user.email,
+        subject: `Bem-vindo ao meu clone do Twitter, ${user.name}`,
+        template: 'auth/register',
+        context: {
+          name: user.name,
+          username: user.username,
+        },
+      });
 
       return res.json({
         user,
